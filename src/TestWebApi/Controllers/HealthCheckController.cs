8c8b1345aae70e4c26988d018fa070fc3fa970d5
mikelau13 <mikelau13@hotmail.com>
2020-11-22 21:32:15 -0500

    added index.html and healthcheck

---
+++ src/TestWebApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestWebApi.Controllers
+{
+    [Route("api/healthcheck")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "succeed";
+        }
+    }
+}
